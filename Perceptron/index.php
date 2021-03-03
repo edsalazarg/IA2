ec29dadba88146a7352f38b21b788b3072fdf1de
Eduardo Salazar <jesus.sgomez@alumnos.udg.mx>
2021-03-02 23:25:11 -0600

    Initial perceptron

---
+++ Perceptron/index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.css">
+
+    <title>Perceptron</title>
+
+</head>
+<body>
+<div class="col-lg-12" style="padding-top: 20px">
+    <div class="card">
+        <div class="card-header">
+            Perceptron
+        </div>
+        <div class="card-body">
+            <div class="col-lg-5">
+                <canvas id="myChart" width="2" height="1"></canvas>
+            </div>
+            <div class="col-lg-5">
+                <button class="btn btn-primary" onclick="showValues()">MostrarValores</button>
+
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
+<script src="main.js"></script>
---
+++ Perceptron/main.js
@@ -0,0 +1,104 @@
+
+
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'scatter',
+    data: {
+        datasets: [{
+            label: 'Scatter Dataset',
+            data: [],
+            backgroundColor: '#FF0000',
+        },
+        {
+            label: 'Scatter Dataset',
+            data: [],
+            backgroundColor: '#0040ff'
+        },
+        {
+            label: 'Line Dataset',
+            data: [{
+                x: -2,
+                y: -2
+            },{
+                x: 2,
+                y: 2
+            }],
+
+            showLine: true,
+
+            fill: false,
+
+            backgroundColor: '#fff200',
+
+            borderColor: '#fff200',
+        }
+        ]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: "linear",
+                display: true,
+                ticks: {
+                    max: 5,
+                    min: -5,
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: 'X-Axis'
+                }
+            }, ],
+            yAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Y-Axis'
+                },
+                ticks: {
+                    max: 5,
+                    min: -5,
+                },
+            }]
+        }
+    }
+});
+
+document.getElementById('myChart').onmousedown = FuncOnClick;
+
+function FuncOnClick(event) {
+    let scaleRef,
+        valueX,
+        valueY;
+    for (var scaleKey in myChart.scales) {
+        scaleRef = myChart.scales[scaleKey];
+        if (scaleRef.isHorizontal() && scaleKey === 'x-axis-1') {
+            valueX = scaleRef.getValueForPixel(event.offsetX);
+
+        } else if (scaleKey === 'y-axis-1') {
+            valueY = scaleRef.getValueForPixel(event.offsetY);
+        }
+    }
+    if (valueY < 5 && valueY > -5 && valueX < 5 && valueX > -5 ){
+        switch (event.which) {
+            case 1:
+                myChart.data.datasets[0].data.push({
+                    x: valueX,
+                    y: valueY
+                });
+                myChart.update();
+                break;
+            case 3:
+                myChart.data.datasets[1].data.push({
+                    x: valueX,
+                    y: valueY
+                });
+                myChart.update();
+                break;
+        }
+    }
+}
+
+// function showValues() {
+//     var values = myChart.data.datasets[2].data.
+//     alert(values)
+// }
